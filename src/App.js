e2743307cc1478db149999daba5064ed30f92d69
Anna <annardwn@gmail.com>
2023-03-05 18:12:23 +1100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
+import CrochetBox from './CrochetBox';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Anna's Crochet Closet</h1>
+
       </header>
+
+      <div className="content">
+            <CrochetBox title= "Double Crochet (DC)" vid= "https://www.youtube.com/embed/5xKssKskNzo?clip=Ugkxg2wQVLzC4MUzrcKSsd3u1FkzfGvUKM1W&amp;clipt=EOmMBRijqgc" />
+            <CrochetBox title= "Half Double Crochet (HDC)" vid="https://www.youtube.com/embed/ajIZThzJ36Q?clip=UgkxlktsgQrklBw-gp1I-Qf8o393I8ZIFeRi&amp;clipt=EN2ECxi7_ww"/>
+            <CrochetBox title="Magic Ring/Circle" vid="https://www.youtube.com/embed/p298HxgsO1s?clip=UgkxgjnVrC9g1qNGALg2pnNri2GV4zJPguxv&amp;clipt=EMGaARinvQM" />
+            <CrochetBox title="dc2tog" />
+            <CrochetBox />
+        </div>
     </div>
   );
 }
 
 export default App;
+
